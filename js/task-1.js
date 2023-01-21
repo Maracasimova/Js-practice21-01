8c4a6dcb96d5bea45881d913900ca0a10cc90c8b
Casimova Marina <maracasimova@gmail.com>
2023-01-21 18:59:46 +0200

    update task1

--- js/task-1.js
+++ js/task-1.js
@@ -6,3 +6,11 @@
  *?     то показать через alert: "Верно!"
  *?     в противном случае отобразить:"Не знаете? ECMAScript!"
  */
+
+const value = prompt("Какое официальное название JavaScript?");
+if (value === 'ECMAScript') {
+    alert("Верно!")
+}
+else {
+    alert("Не знаете? ECMAScript!");
+}
