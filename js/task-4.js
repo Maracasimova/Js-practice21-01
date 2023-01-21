0a0a629198481c98c05c07ff32a435b61e5a6182
Omelianov Nicolai <omelianov.n@gmail.com>
2023-01-21 19:50:05 +0200

    tasc4

--- js/task-4.js
+++ js/task-4.js
@@ -1,6 +1,15 @@
 /**
- *?     При помощи цикла for сложите все четные числа от  min до max
+ *?     При помощи цикла for сложите 
+        все четные числа от  min до max
  */
 
-const max = 50;
+const max = 3;
 const min = 0;
+
+let total = 0;
+for (let i = min; i <= max; i++) {
+    if (i % 2 === 0) {
+        total += i;
+    }
+}
+console.log(total);
