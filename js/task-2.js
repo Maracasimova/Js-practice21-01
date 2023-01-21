27a6ca35a8d661e8d4abaff15773c477e6707807
Omelianov Nicolai <omelianov.n@gmail.com>
2023-01-21 19:26:45 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -5,3 +5,13 @@
  *?      https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
  *?      70 === 01:10
  */
+
+
+let time = +prompt("Введите время в минутах");
+let timeHours = Math.floor(time / 60);
+let modifyHours = String(timeHours).padStart(2, "0");
+let timeMinutes = time % 60;
+let modifyMinutes = String(timeMinutes).padStart(2, "0");
+
+console.log(timeHours);
+console.log(`${modifyHours}:${modifyMinutes}`);
