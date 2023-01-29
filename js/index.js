b909c26648c0e32dc6c545dcf6bd355076509efc
Omelianov Nicolai <omelianov.n@gmail.com>
2023-01-29 14:41:44 +0200

    added task-2

--- js/index.js
+++ js/index.js
@@ -1,25 +1,51 @@
 /**
- *? Напиши скрипт, который для объекта user,
- *? последовательно:
- *? 1 добавит поле mood со значением 'happy'
- *? 2 заменит hobby на 'skydiving'
- *? 3 заменит значение premium на false
- *? 4 выводит содержимое объекта users в фортмате
- *? ключ:значение используя Object.keys() и for...of
+//  *? Напиши скрипт, который для объекта user,
+//  *? последовательно:
+//  *? 1 добавит поле mood со значением 'happy'
+//  *? 2 заменит hobby на 'skydiving'
+//  *? 3 заменит значение premium на false
+//  *? 4 выводит содержимое объекта users в фортмате
+//  *? ключ:значение используя Object.keys() и for...of
+//  */
+
+// const user = {
+//   name: "John",
+//   age: 20,
+//   hobby: "tennis",
+//   premium: true,
+// };
+
+// user.mood = "happy";
+// user.hobby = "skydiving";
+// user.premium = false;
+
+// const array = Object.keys(user);
+// for (const key of array) {
+//     console.log(`${key}:${user[key]}`);
+// }
+
+
+// !2=====================================
+
+
+/**
+ *? У нас есть объект, в котором хранятся зарплаты
+ *? нашей команды
+ *? Напишите код для суммирования всех зарплат и
+ *? сохраните его результат в переменной sum.
+ *? Если объект salaries пуст, то результат должен быть 0
  */
 
-const user = {
-  name: "John",
-  age: 20,
-  hobby: "tennis",
-  premium: true,
+const salaries = {
+  Mango: 100,
+  Poly: 160,
+  Ajax: 1470,
 };
 
-user.mood = "happy";
-user.hobby = "skydiving";
-user.premium = false;
+let sum = 0;
 
-const array = Object.keys(user);
-for (const key of array) {
-    console.log(`${key}:${user[key]}`);
+for (const salary of Object.values(salaries)) {
+  sum += salary;
+
 }
+console.log(sum);
