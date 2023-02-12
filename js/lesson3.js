31b35b2d48bc637fbd41e1ed845a3bfdca0603bc
Casimova Marina <maracasimova@gmail.com>
2023-02-12 15:56:36 +0200

    Update lesson3.js

--- js/lesson3.js
+++ js/lesson3.js
@@ -11,16 +11,53 @@
 
  */
 
-const User = function (obj) {
-    const { userName, age, numberOfPost } = obj;
-    this.userName = userName;
-    this.age = age;
-    this.numberOfPost = numberOfPost;
+// const User = function (obj) {
+//     const { userName, age, numberOfPost } = obj;
+//     this.userName = userName;
+//     this.age = age;
+//     this.numberOfPost = numberOfPost;
 
-    this.getInfo = function () {
-        return  `Пользователю ${this.userName} ${this.age} лет и у него ${this.numberOfPost} публикаций.`
-    }
-}
+//     this.getInfo = function () {
+//         return  `Пользователю ${this.userName} ${this.age} лет и у него ${this.numberOfPost} публикаций.`
+//     }
+// }
 
-const student = new User({ userName: "Petya", age: 21, numberOfPost: 5 });
-console.log(student.getInfo())
+// const student = new User({ userName: "Petya", age: 21, numberOfPost: 5 });
+// console.log(student.getInfo())
+
+
+/**
+ *? Напиши функцию конструктор Storage создающий объекты
+ *? Для управления складом товаров.
+ *? При вызове будет получать один агрумент – начальный массив товаров ["apple", "banana", "mango"],
+ *? и записывать его в свойство items.
+ *? Добавь методы класса:
+ *? getItems() – возвращайте массив товаров
+ *? addItems(item) - получает новый товар и добавляет его к текущим
+ *? removeItem(item) – получает товар и, если он есть, удаляет его из текущих
+ */
+
+// const Storage = function (items) {
+//     this.items = items;
+
+//     this.getItems = function () {
+//         return this.items;
+//     }
+
+//     this.addItems = function (item) {
+//         this.items.push(item);
+//     }
+
+//     this.removeItem = function (item) {
+//         const index = this.items.indexOf(item);
+//         if (index !== -1) {
+//             this.items.splice(index, 1);
+//         }
+//     }
+// }
+
+// const frutsItems = new Storage(["apple", "banana", "mango"]);
+// console.log(frutsItems.removeItem("banana"));
+// console.log(frutsItems.addItems("kiwi"));
+
+// console.log(frutsItems.getItems());
